41844886d440a0a7d821ae738e6fb76be5e54a57
SriGayatri10 <srigayatrimn@gmail.com>
2021-07-21 15:31:18 +0000

    Creating 3boxes.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@
 import { React } from 'react';
 import './App.scss';
 
+// eslint-disable-next-line max-lines-per-function
 const App = () => {
 	// eslint-disable-next-line no-unused-vars
 	const a = 0;
 
 	return <div style={ {
-
-		height: '100px',
-		width: '100px',
+		position: 'absolute',
+		height: '300px',
+		width: '300px',
 		border: '2px solid black',
-		top: '200px',
-		left: '200px',
-	} }
-	       >Hello</div>;
+		top: '30%',
+		left: '40%',
+		backgroundColor: 'green',
+	  } }
+		      >
+		<div style={ {
+			position: 'absolute',
+			height: '200px',
+			width: '200px',
+		  border: '2px solid black',
+			top: '15%',
+			left: '15%',
+			backgroundColor: 'blue',
+				 } }
+		>
+			<div style={ {
+				position: 'absolute',
+	      height: '100px',
+				width: '100px',
+				border: '2px solid black',
+			  top: '20%',
+				left: '20%',
+				backgroundColor: 'crimson',
+				 } }
+			/>
+		</div>
+	</div>;
 };
 
 export default App;
