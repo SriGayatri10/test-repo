7cf8145f26321df034af686b361573e7883eab78
SriGayatri10 <srigayatrimn@gmail.com>
2021-07-27 16:38:59 +0000

    Displaying peoplecount.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { React } from 'react';
 import './App.scss';
-
+import data from './data';
+import People from './components/people';
 // eslint-disable-next-line max-lines-per-function
 const App = () => {
-	// eslint-disable-next-line no-unused-vars
-	const a = 0;
+	console.log(data);
 
-	return <div style={ {
-		position: 'absolute',
-		height: '300px',
-		width: '300px',
-		border: '2px solid black',
-		top: 'calc(50% - 150px)',
-		left: 'calc(50% - 150px)',
-		backgroundColor: 'blue',
-	  } }
-		      >
-		<div style={ {
-			position: 'absolute',
-			height: '200px',
-			width: '200px',
-		  border: '2px solid black',
-			top: 'calc(50% - 100px)',
-			left: 'calc(50% - 100px)',
-			backgroundColor: 'crimson',
-				 } }
-		>
-			<div style={ {
-				position: 'absolute',
-	      height: '100px',
-				width: '100px',
-				border: '2px solid black',
-			  top: 'calc(50% - 50px)',
-				left: 'calc(50% - 50px)',
-				backgroundColor: 'green',
-				 } }
-			/>
-		</div>
-	</div>;
+	return <div className="App">
+		{People(data.people)}</div>;
+	// eslint-disable-next-line no-unused-vars
 };
 
 export default App;
