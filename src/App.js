3839379db34dbea366a2bceacf9cf957479e25e6
SriGayatri10 <srigayatrimn@gmail.com>
2021-07-29 13:20:50 +0000

    Map function to normal function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-console */
-/* eslint-disable no-mixed-spaces-and-tabs */
+
 import { React } from 'react';
 import './App.scss';
 import data from './data';
 import People from './components/people';
-// eslint-disable-next-line max-lines-per-function
+
 const App = () => {
-	console.log(data);
-	// eslint-disable-next-line no-unused-vars
 	const { people } = data;
 
 	return <div className="App">
-		{people.map(People)}</div>;
-	// eslint-disable-next-line no-unused-vars
+		{People(people)}</div>;
 };
 
 export default App;
