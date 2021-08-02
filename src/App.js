609ce9b013a9cfd8033551c383ad4ca8733eb563
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-02 08:07:47 +0000

    Creating buttons.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-magic-numbers */
 
 import { React } from 'react';
 import './App.scss';
-import data from './data';
-import People from './components/people';
+import Button from './components/Button';
 
-const App = () => {
-	const { people } = data;
-
-	return <div className="App">
-		{People(people)}</div>;
-};
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const App = () => <div> {numbers.map(Button)} </div>;
 
 export default App;
