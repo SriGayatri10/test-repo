af27fdfc444eefc78ba41c5ea375fcfbeca7b3a1
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-02 13:23:41 +0000

    Added operators.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@
 import { React } from 'react';
 import './App.scss';
 import Button from './components/Button';
+import Operator from './components/Operators';
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const App = () => <div> {numbers.map(Button)} </div>;
+const operators = ['+', '-', '*', '/'];
+const App = () => <div>
+	<div> {numbers.map(Button)} </div>;
+	<div>{operators.map(Operator)}</div>;
+</div>;
 
 export default App;
