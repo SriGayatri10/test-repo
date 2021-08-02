8a0902424c5b7ac51ab404a075e83a44cefef5ab
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-02 08:08:58 +0000

    Buttons-9 created.

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { React } from 'react';
+
+const Button = (eachButton) =>
+	<button type="button">{eachButton}</button>
+	// console.log(eachButton);
+;
+
+export default Button;
