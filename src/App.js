adbef47c0f7edd0bae5dc79437af4d57b97836a5
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-04 13:18:51 +0000

    Displaying numbers and operators in screen.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-magic-numbers */
@@ -6,12 +7,19 @@ import { React } from 'react';
 import './App.scss';
 import Button from './components/Button';
 import Operator from './components/Operators';
+import context from './core/context';
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 const operators = ['+', '-', '*', '/'];
-const App = () => <div className="layer">
-	<div> {numbers.map(Button)} </div>;
-	<div>{operators.map(Operator)}</div>;
-</div>;
+
+const App = () => {
+	console.log(context.state);
+	return <div className="layer">
+		<div> {numbers.map(Button)} </div>
+		<div>{operators.map(Operator)}</div>
+		<div>{context.state.count}</div>
+		<div>{context.state.operator}</div>
+	</div>;
+};
 
 export default App;
