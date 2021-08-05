3c62a20f5b507b5aac5f5c6c5bd13227470509cd
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-05 12:53:54 +0000

    Tried with colors & images.

--- src/components/person.js
+++ src/components/person.js
@@ -1,13 +1,19 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { React } from 'react';
+// import img from '../logo192.png';
 const Person = (people) => {
 	const { name } = people;
 	const { age } = people;
+	//  const { style } = people;
 	const { color } = people;
+	// const { background } = people;
 
 	return <div
 		style={ {
-			backgroundColor: color,
+			background: color,
+			// fontFamily: style,
+			// background: URL('https://bit.ly/3j8C3W1'),
 		} }
 	       >Hello! {name} . Your age is {age}</div>;
 };
---
+++ src/data.js
@@ -0,0 +1,27 @@
+const data = {
+	people: [
+		{
+			name: 'Ram',
+			age: 22,
+			color: 'Red',
+			// style: 'Gill Sans MT',
+			// background: 'https://bit.ly/3j8C3W1',
+		},
+		{
+			name: 'Arun',
+			age: 22,
+			color: 'Blue',
+			// style: 'serif',
+			// background: 'https://bit.ly/3j8C3W1',
+		},
+		{
+			name: 'Azar',
+			age: 23,
+			color: 'Green',
+			// style: 'Trebuchet MS',
+			// background: 'https://bit.ly/3j8C3W1',
+		},
+	],
+};
+
+export default data;
