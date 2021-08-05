42a8575451a9f77b64215f0a76958b6434a3503d
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-05 13:44:55 +0000

    Created text-box.

--- src/App.js
+++ src/App.js
@@ -6,23 +6,12 @@
 
 import { React } from 'react';
 import './App.scss';
-import Button from './components/Button';
-import Operator from './components/Operators';
 import context from './core/context';
 import Input from './components/Input';
 
-const numbers = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]];
-const operators = ['+', '-', '*', '/'];
-
 const App = () => {
 	console.log(context.state);
-	return <div className="layer">
-		<div>{numbers.map(Button)} </div>
-		<div>{operators.map(Operator)}</div>
-		<div>{context.state.count}</div>
-		<div>{context.state.operator}</div>
-		<div>{Input()}</div>
-	</div>;
+	return <div className="text-box">{Input()}</div>;
 };
 
 export default App;
