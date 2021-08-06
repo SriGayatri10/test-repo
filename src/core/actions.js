72857295c047b7a70aebe897e7571278d17976df
SriGayatri10 <srigayatrimn@gmail.com>
2021-08-06 12:54:06 +0000

    Assigning obtained data to input.

--- src/core/actions.js
+++ src/core/actions.js
@@ -4,9 +4,13 @@ const setCount = ({ data }) => ({
 const setOperator = ({ data }) => ({
 	operator: data,
 });
+const inputs = ({ data }) => ({
+	input: data,
+});
 const actions = {
 	setCount,
 	setOperator,
+	inputs,
 };
 
 export default actions;
